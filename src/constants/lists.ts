a9728ec247a6237fe664c1c7f26f5880c6198098
chwong-tradefi <66941477+chwong-tradefi@users.noreply.github.com>
2020-11-13 17:24:16 +0800

    changes

---
+++ src/constants/lists.ts
@@ -0,0 +1,8 @@
+// the TTSwap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'tokens.ttswap.eth'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  'http://localhost:3000/tokenlist.json'
+]
+
